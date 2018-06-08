0b298b37bd54002240471567088f872667851f51
Suzy home <skw32@bath.ac.uk>
2018-06-08 11:08:21 +0100

    Small updates to Step1 script

--- Step3_plot_band_alignment.py
+++ Step3_plot_band_alignment.py
@@ -1,4 +1,5 @@
 # Ask Keith for e.g. matplotlib script for band alignment diagrams
+# Or use: https://github.com/utf/bapt
 
 # Enter if absorber is p- or n-type - set plot colours based on this (red for p-type absorber, blue for junction partner candidates and vice versa to match write up)
 
@@ -6,3 +7,7 @@
 # Enter EA and IPs of junction partner candidates
 
 # Generate colour-coded plot for absorber band alignment to all min strain junction partner candidates (gradient of colour linked to min possible strain?)
+
+# Modify this e.g. to have more ids for red and blue colours? - https://github.com/utf/bapt/blob/master/examples/gradients.yaml
+print("This step uses Bapt to generate band alignment plots")
+print("Before continuing, please follow installation steps found here: https://github.com/utf/bapt")

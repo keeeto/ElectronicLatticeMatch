d5d942ce2b8a3c2991ad08077688461b52fcb5af
Suzy <skw32@bath.ac.uk>
2018-06-12 15:45:59 +0100

    Updated Step1 and Step2, tidied plan for Step3

--- Step2_min_strain_candidates.py
+++ Step2_min_strain_candidates.py
@@ -1,38 +1,65 @@
-#### OLD SCRIPT ####
+#### This script uses python3 ####
 
 # Adapt below to loop nicely over user-inputted list of non-polar surface terminated for the absorber layer
-# Then loop over all low index surfaces of candidate junction partners, allowing up to 10 unit cells to construct interface (limit) and strain tolerance of 4% 
-
+# Then loop over all low index surfaces of candidate junction partners, allowing up to 10 unit cells to construct interface (limit) and strain tolerance of 4%
 
 import els as els
 import ase.io as io
+from Step1_find_junction_partners import write_slow
 
-candidate = io.read('candidate_cifs/Ce2O3/MyBaseFileName_621711.cif')
-CZTS = io.read('CZTS.cif')
+def main():
+    print("")
+    print("Hello, this is Step2 for using ELS to find candidate heterojunction partners for your absorber material!")
+    print("Please ensure you have a cif file for the bulk structure of your absorber material in this directory.")
+    print("Please also add cif files for the bulk structures of all of your candidate junction partners from Step1 into a directory called candidates.")
+    absorber_file = input("Please enter the name of the cif file for your absorber: ")
+    miller1 = input("Please enter the Miller indices of your absorber one at a time: ")
+    miller1 = int(miller1)
+    miller2 = input("Next index: ")
+    miller2 = int(miller2)
+    miller3 = input("And the last index: ")
+    miller3= int(miller3)
+    candidate = input("Please enter the name of the candidate junction partner you want to screen by strain: ")
+    candidate_cif_file = input("Now please enter the path to the cif file for this candidate from the current directory: ")
+    tol = input("Please enter a tolerance for max. interface strain (or default of 0.04 for 4% will be used): ")
+    if tol == "":
+        tol = 0.04
+    lim = input("Please enter an upper limit for the number of unit cells used to create the interface (otherwise a default of 10 will be used): ")
+    if lim == "":
+        lim = 10
+    output_file = input("And lastly, please choose a name for the file containing your low-strain candidates: ")
+    print("")
+    write_slow("Checking low index terminations of candidate junction partner (strain <= "+str(tol*100)+"%)...")
+    print("")
 
-epitaxy_1, scA_1, scB_1, strains_1 = els.epitaxy_search(CZTS, [1,0,0], candidate, [1,0,0], tolerance=0.04, limit=10)
-print(epitaxy_1)
+    absorber = io.read(absorber_file)
+    outputs = open(output_file, "w")
+    outputs.write("junction partner, miller indices of partner, unit cells of absorber, unit cells of candidate partner, strains\n")
 
-epitaxy_2, scA_2, scB_2, strains_2 = els.epitaxy_search(CZTS, [1,0,0], candidate, [1,1,0], tolerance=0.04, limit=10)
-print(epitaxy_2)
+    # Need to figure out how to loop over candidate dirs or just ask user to input path??
+    #candidate_cif = io.read('candidates/Ce2O3/MyBaseFileName_621711.cif')
+    candidate_cif = io.read(candidate_cif_file)
+    # Loop over all low index terminations for junction partner candidate
+    for m in range(0,2):
+        for k in range(0,2):
+            for l in range(0,2):
+                if not (m==0 and k ==0 and l == 0):
+                    epitaxy, sc_ab, sc_part, strains = els.epitaxy_search(absorber, [miller1, miller2, miller3], candidate_cif, [m, k, l], tolerance=tol, limit=lim)
+                    if epitaxy:
+                        outputs.write(candidate+" ")
+                        outputs.write("(")
+                        outputs.write(str(m))
+                        outputs.write(str(k))
+                        outputs.write(str(l))
+                        outputs.write(") ")
+                        outputs.writelines(str(sc_ab)+" ")
+                        outputs.writelines(str(sc_part)+" ")
+                        outputs.writelines(str(strains)+" ")
+                        outputs.write("\n")
 
-epitaxy_3, scA_3, scB_3, strains_3 = els.epitaxy_search(CZTS, [1,0,0], candidate, [1,0,1], tolerance=0.04, limit=10)
-print(epitaxy_3)
+    outputs.close()
 
-epitaxy_4, scA_4, scB_4, strains_4 = els.epitaxy_search(CZTS, [1,0,0], candidate, [0,0,1], tolerance=0.04, limit=10)
-print(epitaxy_4)
+    print("Please check your output file.")
 
-epitaxy_5, scA_5, scB_5, strains_5 = els.epitaxy_search(CZTS, [1,0,0], candidate, [0,1,1], tolerance=0.04, limit=10)
-print(epitaxy_5)
-
-epitaxy_6, scA_6, scB_6, strains_6 = els.epitaxy_search(CZTS, [1,0,0], candidate, [1,1,1], tolerance=0.04, limit=10)
-print(epitaxy_6)
-
-
-# outputs for true
-print(scA_1, scB_1, strains_1)
-print(scA_2, scB_2, strains_2)
-print(scA_3, scB_3, strains_3)
-print(scA_4, scB_4, strains_4)
-print(scA_5, scB_5, strains_5)
-#print(scA_6, scB_6, strains_6)
+if __name__ == '__main__':
+    main()
--- Step3_plot_band_alignment.py
+++ Step3_plot_band_alignment.py
@@ -1,13 +1,15 @@
 # Ask Keith for e.g. matplotlib script for band alignment diagrams
 # Or use: https://github.com/utf/bapt
 
-# Enter if absorber is p- or n-type - set plot colours based on this (red for p-type absorber, blue for junction partner candidates and vice versa to match write up)
-
-# Enter EA and IP of absorber
-# Enter EA and IPs of junction partner candidates
-
-# Generate colour-coded plot for absorber band alignment to all min strain junction partner candidates (gradient of colour linked to min possible strain?)
-
 # Modify this e.g. to have more ids for red and blue colours? - https://github.com/utf/bapt/blob/master/examples/gradients.yaml
-print("This step uses Bapt to generate band alignment plots")
+print("Step3 uses Bapt to generate band alignment plots from candidate junction partners identified with ELS")
 print("Before continuing, please follow installation steps found here: https://github.com/utf/bapt")
+input("Press Enter to continue...\n")
+
+print("We will now plot band alignments for your absorber using the minimum strain candidates identified in Step2.")
+# Ask user if absorber was p- or n-type 
+# (for colour coding plot red for p-type absorber with blue contacts, blue for n-type absorber with red contacts)
+# Ask user for name of absorber, IP and Eg
+# Ask user for total number of junction partner candidates
+# Ask user for name of each junction partner and IP and EA from Step1 output file and min strain of candidate from Step2
+# Use strain to assign colour gradient to plot, darker for lower strain?
